245f1a0b69d20f636f2a062d853940ff70922ae6
Wojciech Grabowski <wojtek@wojciech-grabowski.pl>
2021-07-01 11:20:57 +0200

    Add basic README. Improver outout formatting

---
+++ README.md
@@ -0,0 +1,17 @@
+# ngx-unused
+Find declared but unused Angular code.
+
+# Usage 
+Simplest way to use is via npx:
+`npx ngx-unused <tsconfig-path>`
+
+`<tsconfig-path>` - path to tsconfig file _(note: files excluded in given config will not be analyzed)_
+
+This project is on early stage so using `@latest` version tag is recommended:
+`npx ngx-unused@latest <tsconfig-path>`
+
+
+
+
+
+
--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-const packageJson = require('./package.json');
-import { exit } from "process";
+const packageJson = require("./package.json");
+import { exit, stdout } from "process";
 import { Node } from "ts-morph";
 import {
   getProjectFiles,
@@ -10,20 +10,23 @@ import {
   NgModuleMetadataField,
   ComponentSourceFile,
 } from "./packages/ast/src";
-console.log(`[${packageJson.name} v.${packageJson.version}]`)
-if(process.argv.length < 3){
-    console.log("You have to provide path to tsconfig file");
-    exit();
+console.log(`[${packageJson.name} v.${packageJson.version}]`);
+if (process.argv.length < 3) {
+  console.log("You have to provide path to tsconfig file");
+  exit();
 }
+stdout.write("Analyzing files. Be patient, it can take some time\n");
 const files = getProjectFiles(process.argv[2]);
 const componentFiles: ComponentSourceFile[] = getComponentFiles(files);
 const selectorUsages = getComponentUsagesBySelector(componentFiles);
 const classNameUsages = getComponentUsagesByClassName(componentFiles);
 
-const usages = componentFiles.map(({ file }) => ({
+const usages = componentFiles.map(({ file, className, componentSelector }) => ({
   filePath: file.getFilePath(),
   selectorUsage: selectorUsages[file.getFilePath()],
   classUsage: classNameUsages[file.getFilePath()],
+  className,
+  componentSelector,
   probablyUnused:
     selectorUsages[file.getFilePath()] + classNameUsages[file.getFilePath()] <=
     0,
@@ -59,9 +62,31 @@ function getComponentUsagesByClassName(componentFiles: ComponentSourceFile[]) {
   }, {} as any);
 }
 
-console.log('Probably unused components:')
-console.log(
-  usages
-    .filter(({ probablyUnused }) => probablyUnused)
-    .map(({ filePath }) => filePath).join("\n")
+
+const unusedComponents = usages.filter(({ probablyUnused }) => probablyUnused);
+// temporary outout formatting
+const maxClassNameLength = Math.max(
+  ...unusedComponents.map((file) => file.className.length)
 );
+const maxSelectorLength = Math.max(
+  ...unusedComponents.map((file) => file.className.length)
+);
+
+if (unusedComponents.length) {
+  stdout.write(`\n${unusedComponents.length} probably unused component(s):\n`);
+  stdout.write(
+    `${"Class name".padEnd(maxClassNameLength)} | ${"Selector".padEnd(
+      maxSelectorLength
+    )} | file path\n`
+  );
+
+  unusedComponents.forEach((file) => {
+    stdout.write(
+      `${file.className.padEnd(
+        maxClassNameLength
+      )} | ${file.componentSelector.padEnd(maxSelectorLength)} | ${
+        file.filePath
+      }\n`
+    );
+  });
+}
