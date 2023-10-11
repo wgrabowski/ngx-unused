ef85c381be3de09fb951944be1723a4e10c8fea0
Wojciech Grabowski <wojtek@wojciech-grabowski.pl>
2023-10-11 23:43:11 +0200

    feat(cli): initial cli setup with args validationa and help prompt

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # Version 2.0.0 is in works
 
 # Docmentation for version 1.0.0
+
 # ngx-unused
+
 Find declared but unused Angular code.
 
 This tool recognizes components, directives, pipes and services in Angular code and cheks if they are used in provided project.
 
 # Usage
+
 Simplest way to use is via npx:
 
 `npx ngx-unused <tsconfig-path>`
@@ -14,6 +17,7 @@ Simplest way to use is via npx:
 `<tsconfig-path>` - path to tsconfig file _(note: files excluded in given config will not be analyzed)_
 
 # Important notes
+
 Component - is a class with `Component` decorator.
 
 Directive - is a class with `Directive` decorator.
--- index.ts
+++ index.ts
@@ -1,1 +1,42 @@
-console.log('ngx-unused');
+import { existsSync } from 'fs';
+import minimist, { ParsedArgs } from 'minimist';
+import { join } from 'path';
+import { argv, cwd, exit, stdout } from 'process';
+
+interface CliArgs extends ParsedArgs {
+	help?: boolean;
+	h?: boolean;
+}
+
+function help() {
+	stdout.write('usage: ngx-unused [<directory>] [-h | --help]\n\n');
+	stdout.write('<directory> - root directory of Angular workspace\n');
+	stdout.write('              (optional, current directory is default)\n');
+	stdout.write('--help,-h   - show this help\n');
+}
+
+const cliArgs: CliArgs = minimist(argv.slice(2));
+
+if (cliArgs.help || cliArgs.h) {
+	help();
+	exit(0);
+}
+
+const workspaceDir = cliArgs._.length === 1 ? cliArgs._[0] : cwd();
+const workspaceDirExists = existsSync(workspaceDir);
+const isAngularWorkspace = existsSync(join(workspaceDir, 'angular.json'));
+
+if (!workspaceDirExists) {
+	stdout.write(`Directory ${workspaceDir} doesn't exists.`);
+	exit(2);
+}
+
+if (!isAngularWorkspace) {
+	stdout.write(
+		`Directory ${workspaceDir} is not an Agular workspace root directory.\n`
+	);
+	stdout.write('Directory must contain angular.json file.\n');
+	exit(2);
+}
+
+stdout.write('Checking files...\n'); // TODO implementation goes there
